085111cb34bd76f46c43c40898dfbdb0e5d668f1
sabreen-parveen <sabreenparween2001@gmail.com>
2022-09-22 20:50:43 +0530

    initial commit

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">lamabooking</span>
+        </Link>
+        {user ? (
+          user.username
+        ) : (
+          <div className="navItems">
+            <button
+              className="navButton"
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              Register
+            </button>
+            <button
+              className="navButton"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Login
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
